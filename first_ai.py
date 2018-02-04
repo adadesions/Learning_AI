441917bf041357ecfbbfe4fbd898030eb95b4b1e
adadesions <adadesions@gmail.com>
2018-02-04 10:25:53 +0700

    Ready

--- first_ai.py
+++
@@ -1,5 +0,0 @@
-from sklearn import datasets 
-
-house_price = datasets.load_boston()
-digits = datasets.load_digits()
-print(digits.images[0])
--- login_bot.py
+++ login_bot.py
@@ -19,19 +19,19 @@ time.sleep(0.5)
 
 email = driver.find_element_by_name('email')
 password = driver.find_element_by_name('password')
-captcha_ele = driver.find_element_by_xpath('//*[@id="atk-modal-login"]/div/div/div/form/div[4]/div[1]/img[1]')
-captcha_location = captcha_ele.location
-captcha_size = captcha_ele.size
-driver.save_screenshot('wholescreen.png')
+# captcha_ele = driver.find_element_by_xpath('//*[@id="atk-modal-login"]/div/div/div/form/div[4]/div[1]/img[1]')
+# captcha_location = captcha_ele.location
+# captcha_size = captcha_ele.size
+# driver.save_screenshot('wholescreen.png')
 
-screen = Image.open('wholescreen.png')
-left = captcha_location['x']
-top = captcha_location['y']
-right = captcha_location['x'] + captcha_size['width']
-bottom = captcha_location['y'] + captcha_size['height']
+# screen = Image.open('wholescreen.png')
+# left = captcha_location['x']
+# top = captcha_location['y']
+# right = captcha_location['x'] + captcha_size['width']
+# bottom = captcha_location['y'] + captcha_size['height']
 
-screen = screen.crop((left, top, right, bottom))
-screen.save('captcha.png')
+# screen = screen.crop((left, top, right, bottom))
+# screen.save('captcha.png')
 
 time.sleep(0.3)
 # im = Image.open('./captcha.png')
@@ -40,8 +40,8 @@ time.sleep(0.3)
 # im = enhancer.enhance(2)
 # im = im.convert('1')
 # im.save('mod_captcha.png')
-text = pytesseract.image_to_string(Image.open('./captcha.png'), config='tessedit_char_whitelist=0123456789')
-print(text)
+# text = pytesseract.image_to_string(Image.open('./captcha.png'), config='tessedit_char_whitelist=0123456789')
+# print(text)
 
 email.send_keys(sys.argv[1])
 password.send_keys(sys.argv[2])
